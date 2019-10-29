965d4fb400d8388a421686c845f22b5a14a2a5a6
hanchi <lkk123139@gmail.com>
2019-10-29 20:41:54 +0800

    modify index.php version

--- index.php
+++ index.php
@@ -10,9 +10,6 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	<style >
-		div.inner_text{
-			text-indent: 30px;
-		}
 		.our-partners{
 			display: block;
 			clear: both;
@@ -20,7 +17,7 @@
 		h3,h4{
 			text-align:center;
 		}
-		
+
 		.phototitle{
 			background-color: #008B8B; 
 			height: 100px;
@@ -39,7 +36,7 @@
 			margin: 5px;
 			border: 1px solid #ccc;
 			float: left;
-			width: 170px;
+			width: 140px;
 		}
 
 		div.img:hover {
@@ -50,12 +47,20 @@
 			width: 100%;
 			height: auto;
 		}
+		img.img{
+			width:100%;
+			height:100%;
+		}
 		div.desc {
 			padding: 15px;
 			text-align: center;
 		}
+		div.aa{
+					display: block;
+					padding: 2%;
+		}
 	</style>
-	<title>科學少女</title>
+	<title>科學少女xxxxxxxxxxxxxxx</title>
 	<!-- - -->
     <script type="text/javascript">
         jQuery(document).ready(function($) {
@@ -68,8 +73,9 @@
     <!-- -->
 </head>
 <body>
+	<div class="aa">
 	<div class="container">
-		<nav class="navbar navbar-expand-lg navbar-light">
+		<nav class="navbar navbar-expand-lg navbar-light fixed-top bg-dark navbar-dark ">
 				<div class="container-fluid">
 					<div class="navbar-brand">
 						<img src="logo.png" width="50" height="30">
@@ -108,10 +114,11 @@
 				</div>
 			</nav>
 		</div>
+	</div>
 	<div class="our-partners">	
 			<div class="container" id="event">
 				<h3>最新活動</h3>
-		        <div class = "inner_text">據哆啦A夢日本電影官網的消息，《電影哆啦A夢：大雄的新恐龍》已定檔 2020 年 3 月 6 日（星期五）上映。而這是繼電影哆啦A夢在日本再次由《月球探測（險）記》的周六上映，恢復為以往的週五上映慣例（即 3 月初的週五上映哆啦A夢電影）。
+		        <div >	據哆啦A夢日本電影官網的消息，《電影哆啦A夢：大雄的新恐龍》已定檔 2020 年 3 月 6 日（星期五）上映。而這是繼電影哆啦A夢在日本再次由《月球探測（險）記》的周六上映，恢復為以往的週五上映慣例（即 3 月初的週五上映哆啦A夢電影）。
 				</div>
 				<div class="clearfix"></div>
 			</div>	
@@ -143,20 +150,20 @@
         	<div class="row">        		
 			<div class="col-xs-12 col-sm-6 col-md-6 ">
 				<h4>多拉b夢</h4>
-	            <div class = "inner_text">本作主角（原名：ドラえもん），香港稱多啦A夢，來自未來世界（22世紀）的藍色貓型機器人，身上的四次元百寶袋裝有許多道具，為改變大雄的命運而來。因為失去原生的耳朵（電視動畫設定被真正的老鼠咬掉，《2112年哆啦A夢的誕生》設定被機械鼠咬掉），常被人以為是藍色的狸貓而引起不滿。弱點為害怕老鼠，喜歡吃銅鑼燒。
+	            <div >本作主角（原名：ドラえもん），香港稱多啦A夢，來自未來世界（22世紀）的藍色貓型機器人，身上的四次元百寶袋裝有許多道具，為改變大雄的命運而來。因為失去原生的耳朵（電視動畫設定被真正的老鼠咬掉，《2112年哆啦A夢的誕生》設定被機械鼠咬掉），常被人以為是藍色的狸貓而引起不滿。弱點為害怕老鼠，喜歡吃銅鑼燒。
 				</div>
 			</div>
 			<div class="col-xs-12 col-sm-6 col-md-6" >
-				<img src="a_moon1.jpg" width="100%" height="100%">
+				<img src="a_moon1.jpg" class="img">
 			</div>
 			<div class="clearfix"></div>
         	<div class="row"> 
         	<div class="col-xs-12 col-sm-6 col-md-6" >
-				<img src="da_than.jpg" width="100%" height="100%">
+				<img src="da_than.jpg" class="img">
 			</div>       		
 			<div class="col-xs-12 col-sm-6 col-md-6 ">
 				<h4>大熊</h4>
-	            <div class = "inner_text">本名野比大雄（野比のび太）。本作主角，哆啦A夢負責照顧的對象。功課、體育都不好（程度小學二年級），也因個性懦弱、懶惰而常受師長的責罵及同學的霸凌。時常因獲得道具而貪心，但總體心地善良，富有同理心，熱愛自然。擅長射擊、翻花繩和快速入睡(最快記錄為0.93秒)。喜歡靜香。
+	            <div >本名野比大雄（野比のび太）。本作主角，哆啦A夢負責照顧的對象。功課、體育都不好（程度小學二年級），也因個性懦弱、懶惰而常受師長的責罵及同學的霸凌。時常因獲得道具而貪心，但總體心地善良，富有同理心，熱愛自然。擅長射擊、翻花繩和快速入睡(最快記錄為0.93秒)。喜歡靜香。
 				</div>
 			</div>
 			<div class="clearfix"></div>
@@ -167,15 +174,14 @@
 	<div class="our-partners">
 		<div class="container" id="designTeam">
 			<h3>主創團隊</h3>
-            <div class = "inner_text">藤子．F．不二雄不只是大家常知的兒童漫畫家。藤子老師的世界有兩大基礎，「兒童文學與日常 SF」。自 1950 年代後期一直到 1960 年代前期為主的作品，是藤子·F·不二雄在兒童雜誌上站穩基礎並發展自己漫畫特色的重要時代。在藤子·F·不二雄創作的作品中，固然有像《哆啦A夢》這樣面對兒童創作的漫畫，但另一方面他也在《ビッグコミック》這類面對青年或成人的漫畫雜誌推出各式各樣的「SF 異色短篇集」，「SF」是指「一小點的不可思議」的意思，而短篇的內容以漫畫的形式嘲諷現實，因此藤子老師可看的作品絕對不只是《哆啦A夢》，更是一名具有廣度與深度的漫畫家。
+            <div >藤子．F．不二雄不只是大家常知的兒童漫畫家。藤子老師的世界有兩大基礎，「兒童文學與日常 SF」。自 1950 年代後期一直到 1960 年代前期為主的作品，是藤子·F·不二雄在兒童雜誌上站穩基礎並發展自己漫畫特色的重要時代。在藤子·F·不二雄創作的作品中，固然有像《哆啦A夢》這樣面對兒童創作的漫畫，但另一方面他也在《ビッグコミック》這類面對青年或成人的漫畫雜誌推出各式各樣的「SF 異色短篇集」，「SF」是指「一小點的不可思議」的意思，而短篇的內容以漫畫的形式嘲諷現實，因此藤子老師可看的作品絕對不只是《哆啦A夢》，更是一名具有廣度與深度的漫畫家。
 			</div>
 			<div class="clearfix"></div>
 		</div>
 	</div>
 	<div class="our-partners">
 		<div class="container" id="photos">
-			<h3>精彩劇照</h3>
-            <div class = "inner_text">
+			<h3>精彩劇照</h3>           
 				<div id="carousel" class="carousel slide" data-ride="carousel">
 				<!-- 照片數 -->
 					<ul class="carousel-indicators">
@@ -185,18 +191,18 @@
 						<li data-target="#carousel" data-slide-to="3"></li>
 					</ul>
 				<!-- 幻燈片 -->
-					<div class="carousel-inner">
+					<div >
 						<div class="carousel-item active">
-							<img src="image/pic0.jpg">
+							<img src="image/pic0.jpg" class="img">
 						</div>
 						<div class="carousel-item">
-							<img src="image/pic1.jpg">
+							<img src="image/pic1.jpg" class="img">
 						</div>
 						<div class="carousel-item">
-							<img src="image/pic2.jpg">
+							<img src="image/pic2.jpg" class="img">
 						</div>
 						<div class="carousel-item">
-							<img src="image/pic3.jpg">
+							<img src="image/pic3.jpg" class="img">
 						</div>
 					</div>
 				<!-- 左右切換-->
@@ -211,7 +217,7 @@
 				<div class="responsive">
 					<div class="img">
 						<a target="_blank" href="image/pic0.jpg">
-						<img src="image/pic0.jpg" alt="圖片描述" width="300" height="200">
+						<img src="image/pic0.jpg" alt="圖片描述">
 						</a>
 						<div class="desc">精彩劇照1</div>
 					</div>
@@ -219,7 +225,7 @@
 				<div class="responsive">
 					<div class="img">
 						<a target="_blank" href="image/pic1.jpg">
-						<img src="image/pic1.jpg" alt="圖片描述" width="300" height="200">
+						<img src="image/pic1.jpg" alt="圖片描述">
 						</a>
 						<div class="desc">精彩劇照2</div>
 					</div>
@@ -227,7 +233,7 @@
 				<div class="responsive">
 					<div class="img">
 						<a target="_blank" href="image/pic2.jpg">
-						<img src="image/pic2.jpg" alt="圖片描述" width="300" height="200">
+						<img src="image/pic2.jpg" alt="圖片描述">
 						</a>
 						<div class="desc">精彩劇照3</div>
 					</div>
@@ -235,7 +241,7 @@
 				<div class="responsive">
 					<div class="img">
 						<a target="_blank" href="image/pic3.jpg">
-						<img src="image/pic3.jpg" alt="圖片描述" width="300" height="200">
+						<img src="image/pic3.jpg" alt="圖片描述">
 						</a>
 						<div class="desc">精彩劇照4</div>
 					</div>
@@ -243,7 +249,7 @@
 				<div class="responsive">
 					<div class="img">
 						<a target="_blank" href="image/pic4.jpg">
-						<img src="image/pic4.jpg" alt="圖片描述" width="300" height="200">
+						<img src="image/pic4.jpg" alt="圖片描述">
 						</a>
 						<div class="desc">精彩劇照5</div>
 					</div>
@@ -251,7 +257,7 @@
 				<div class="responsive">
 					<div class="img">
 						<a target="_blank" href="image/pic5.jpg">
-						<img src="image/pic5.jpg" alt="圖片描述" width="300" height="200">
+						<img src="image/pic5.jpg" alt="圖片描述">
 						</a>
 						<div class="desc">精彩劇照6</div>
 					</div>
@@ -259,7 +265,7 @@
 				<div class="responsive">
 					<div class="img">
 						<a target="_blank" href="image/pic6.jpg">
-						<img src="image/pic6.jpg" alt="圖片描述" width="300" height="200">
+						<img src="image/pic6.jpg" alt="圖片描述">
 						</a>
 						<div class="desc">幕後花絮1</div>
 					</div>
@@ -267,7 +273,7 @@
 				<div class="responsive">
 					<div class="img">
 						<a target="_blank" href="image/pic7.jpg">
-						<img src="image/pic7.jpg" alt="圖片描述" width="300" height="200">
+						<img src="image/pic7.jpg" alt="圖片描述">
 						</a>
 						<div class="desc">幕後花絮2</div>
 					</div>
@@ -275,7 +281,7 @@
 				<div class="responsive">
 					<div class="img">
 						<a target="_blank" href="image/pic8.jpg">
-						<img src="image/pic8.jpg" alt="圖片描述" width="300" height="200">
+						<img src="image/pic8.jpg" alt="圖片描述">
 						</a>
 						<div class="desc">幕後花絮3</div>
 					</div>
@@ -283,7 +289,7 @@
 				<div class="responsive">
 					<div class="img">
 						<a target="_blank" href="image/pic9.jpg">
-						<img src="image/pic9.jpg" alt="圖片描述" width="300" height="200">
+						<img src="image/pic9.jpg" alt="圖片描述">
 						</a>
 						<div class="desc">幕後花絮4</div>
 					</div>
@@ -299,7 +305,7 @@
 				<div class="responsive">
 					<div class="img">
 						<a target="_blank" href="image/pic11.jpg">
-						<img src="image/pic11.jpg" alt="圖片描述" width="300" height="200">
+						<img src="image/pic11.jpg" alt="圖片描述">
 						</a>
 						<div class="desc">幕後花絮6</div>
 					</div>
@@ -307,19 +313,18 @@
 				<div class="responsive">
 					<div class="img">
 						<a target="_blank" href="image/pic12.jpg">
-						<img src="image/pic12.jpg" alt="圖片描述" width="300" height="200">
+						<img src="image/pic12.jpg" alt="圖片描述">
 						</a>
 						<div class="desc">幕後花絮7</div>
 					</div>
 				</div>
-			</div>
 			<div class="clearfix"></div>
 		</div>
 	</div>
 	<div class="our-partners">
 		<div class="container" id="pictureBook">
 			<h3>音樂繪本</h3>
-            <div class = "inner_text">bbbbb
+            <div >bbbbb
             	<h1>等學長資料</h1>
 			</div>
 			<div class="clearfix"></div>
@@ -328,7 +333,7 @@
 	<div class="our-partners">
 		<div class="container" id="movieNovel">
 			<h3>電影小說</h3>
-            <div class = "inner_text">ccccc
+            <div>ccccc
             	<h1>等老師資料</h1>
 			</div>
 			<div class="clearfix"></div>
