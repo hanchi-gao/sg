8dfe6784feb5f3d9162588836b8b472875464b9f
hanchi <lkk123139@gmail.com>
2019-10-30 21:13:20 +0800

    1.add fixle title line 2.add logo url

---
+++ index.php
@@ -0,0 +1,350 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<style >
+		.our-partners{
+			display: block;
+			clear: both;
+		}
+		h3,h4{
+			text-align:center;
+		}
+
+		.phototitle{
+			background-color: #008B8B; 
+			height: 100px;
+		}
+
+		h1{
+			font-weight:bold;
+			line-height: 100px;
+		}
+
+		.carousel-inner img{
+			width: 100%;
+		}
+
+		div.img {
+			margin: 5px;
+			border: 1px solid #ccc;
+			float: left;
+			width: 140px;
+		}
+
+		div.img:hover {
+			border: 1px solid #777;
+		}
+
+		div.img img {
+			width: 100%;
+			height: auto;
+		}
+		img.img{
+			width:100%;
+			height:100%;
+		}
+		div.desc {
+			padding: 15px;
+			text-align: center;
+		}
+		div.title_line{
+					display: block;
+					padding: 2%;
+		}
+	</style>
+	<title>科學少女xxxxxxxxxxxxxxx</title>
+	<!-- - -->
+    <script >
+        $(document).ready(function() {
+            $("a.nav-link").click(function(){		
+                // event.preventDefault();
+                //alert($(this.hash).offset().top);
+				//$('html,body').animate(scrollTop(this.hash),1000);
+				$('html,body').animate({
+					scrollTop: $(this.hash).offset().top-75
+				});
+            });
+        });
+    </script>
+    <!-- -->
+</head>
+<body>
+	<div class="title_line">
+	<div class="container">
+		<nav class="navbar navbar-expand-lg navbar-light fixed-top bg-dark navbar-dark ">
+				<div class="container-fluid">
+					<div class="navbar-brand">
+						<a href="http://localhost/science_teenage_girl/index2.php">
+						<img src="logo.png" alt="go to index" width="60" height="55">
+						</a>
+					</div>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarText">
+						<ul class="nav navbar-nav mr-auto">
+							<li class="nav-item">
+								<a class="nav-link" href="#about">關於《科學少女》</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#story">劇情大綱</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#cast">人物介紹</a>
+							</li>   
+							<li class="nav-item">
+								<a class="nav-link" href="#designTeam">主創團隊</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#photos">精彩劇照</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#pictureBook">音樂繪本</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#movieNovel">電影小說</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#event">最新活動</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
+	</div>
+	<div class="our-partners">	
+			<div class="container" id="event">
+				<h3 >最新活動</h3>
+		        <div >	據哆啦A夢日本電影官網的消息，《電影哆啦A夢：大雄的新恐龍》已定檔 2020 年 3 月 6 日（星期五）上映。而這是繼電影哆啦A夢在日本再次由《月球探測（險）記》的周六上映，恢復為以往的週五上映慣例（即 3 月初的週五上映哆啦A夢電影）。
+				</div>
+				<div class="clearfix"></div>
+			</div>	
+	</div>
+	<div class="container">
+		<div class="embed-responsive embed-responsive-16by9">
+	  		<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/rj-jFHGKfig" frameborder="0" allowfullscreen></iframe>
+		</div>
+	</div>	
+	<div class="our-partners">
+		<div class="container" id="about">
+			<h3>關於《多拉b夢》</h3>
+            <div class = "inner_text">《哆啦A夢》（日語：ドラえもん）為日本漫畫家藤子·F·不二雄筆下著名的兒童、科幻漫畫作品，自1969年12月開始連載，共發行單行本45冊（未收錄作品參見哆啦A夢+）[1][2]、原著大長篇漫畫17本（加上其弟子所編繪之大長篇共24本)，至今已成為日本國民級長壽動漫。本作主要敘述一隻來自22世紀的貓型機器人——哆啦A夢，受原本主人野比世修的託付，回到20/21世紀，幫助世修的高祖父野比大雄的故事。本作品在日本、亞洲和歐洲知名度高，作品中亦有滲入當時社會問題。1973年，本作首度由日本電視台動畫公司製成動畫播放，半年後因該公司倒閉而終止播出。其後由朝日電視台於1979年起製作播放至今，從1980年起每年3月上映一部動畫電影
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+	<div class="our-partners">
+		<div class="container" id="story">
+			<h3>劇情大綱</h3>
+            <div class = "inner_text">來自未來的哆啦A夢，為了滿足大雄的夢想和願望，拿出未來道具幫助大雄，但常常會引發騷動，大雄、胖虎等人過度依賴未來科學技術的結果，經常在最後會引起不好的效果，這種常出現的結局，被認為是作者想要在作品中傳達「雖然文明進步，但最重要的還是使用的人」的思想。
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+	<div class="our-partners">
+		<div class="container" id="cast">
+			<h3>人物介紹</h3>
+        	<div class="row">        		
+			<div class="col-xs-12 col-sm-6 col-md-6 ">
+				<h4>多拉b夢</h4>
+	            <div >本作主角（原名：ドラえもん），香港稱多啦A夢，來自未來世界（22世紀）的藍色貓型機器人，身上的四次元百寶袋裝有許多道具，為改變大雄的命運而來。因為失去原生的耳朵（電視動畫設定被真正的老鼠咬掉，《2112年哆啦A夢的誕生》設定被機械鼠咬掉），常被人以為是藍色的狸貓而引起不滿。弱點為害怕老鼠，喜歡吃銅鑼燒。
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-6" >
+				<img src="a_moon1.jpg" class="img">
+			</div>
+			<div class="clearfix"></div>
+        	<div class="row"> 
+        	<div class="col-xs-12 col-sm-6 col-md-6" >
+				<img src="da_than.jpg" class="img">
+			</div>       		
+			<div class="col-xs-12 col-sm-6 col-md-6 ">
+				<h4>大熊</h4>
+	            <div >本名野比大雄（野比のび太）。本作主角，哆啦A夢負責照顧的對象。功課、體育都不好（程度小學二年級），也因個性懦弱、懶惰而常受師長的責罵及同學的霸凌。時常因獲得道具而貪心，但總體心地善良，富有同理心，熱愛自然。擅長射擊、翻花繩和快速入睡(最快記錄為0.93秒)。喜歡靜香。
+				</div>
+			</div>
+			<div class="clearfix"></div>
+			</div>
+		</div>
+		</div>
+	</div>
+	<div class="our-partners">
+		<div class="container" id="designTeam">
+			<h3>主創團隊</h3>
+            <div >藤子．F．不二雄不只是大家常知的兒童漫畫家。藤子老師的世界有兩大基礎，「兒童文學與日常 SF」。自 1950 年代後期一直到 1960 年代前期為主的作品，是藤子·F·不二雄在兒童雜誌上站穩基礎並發展自己漫畫特色的重要時代。在藤子·F·不二雄創作的作品中，固然有像《哆啦A夢》這樣面對兒童創作的漫畫，但另一方面他也在《ビッグコミック》這類面對青年或成人的漫畫雜誌推出各式各樣的「SF 異色短篇集」，「SF」是指「一小點的不可思議」的意思，而短篇的內容以漫畫的形式嘲諷現實，因此藤子老師可看的作品絕對不只是《哆啦A夢》，更是一名具有廣度與深度的漫畫家。
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+	<div class="our-partners">
+		<div class="container" id="photos">
+			<h3>精彩劇照</h3>           
+				<div id="carousel" class="carousel slide" data-ride="carousel">
+				<!-- 照片數 -->
+					<ul class="carousel-indicators">
+						<li data-target="#carousel" data-slide-to="0" class="active"></li>
+						<li data-target="#carousel" data-slide-to="1"></li>
+						<li data-target="#carousel" data-slide-to="2"></li>
+						<li data-target="#carousel" data-slide-to="3"></li>
+					</ul>
+				<!-- 幻燈片 -->
+					<div >
+						<div class="carousel-item active">
+							<img src="image/pic0.jpg" class="img">
+						</div>
+						<div class="carousel-item">
+							<img src="image/pic1.jpg" class="img">
+						</div>
+						<div class="carousel-item">
+							<img src="image/pic2.jpg" class="img">
+						</div>
+						<div class="carousel-item">
+							<img src="image/pic3.jpg" class="img">
+						</div>
+					</div>
+				<!-- 左右切換-->
+					<a class="carousel-control-prev" href="#carousel" data-slide="prev">
+						<span class="carousel-control-prev-icon"></span>
+					</a>
+					<a class="carousel-control-next" href="#carousel" data-slide="next">
+						<span class="carousel-control-next-icon"></span>
+					</a>
+				</div>
+
+				<div class="responsive">
+					<div class="img">
+						<a target="_blank" href="image/pic0.jpg">
+						<img src="image/pic0.jpg" alt="圖片描述">
+						</a>
+						<div class="desc">精彩劇照1</div>
+					</div>
+				</div>
+				<div class="responsive">
+					<div class="img">
+						<a target="_blank" href="image/pic1.jpg">
+						<img src="image/pic1.jpg" alt="圖片描述">
+						</a>
+						<div class="desc">精彩劇照2</div>
+					</div>
+				</div>
+				<div class="responsive">
+					<div class="img">
+						<a target="_blank" href="image/pic2.jpg">
+						<img src="image/pic2.jpg" alt="圖片描述">
+						</a>
+						<div class="desc">精彩劇照3</div>
+					</div>
+				</div>
+				<div class="responsive">
+					<div class="img">
+						<a target="_blank" href="image/pic3.jpg">
+						<img src="image/pic3.jpg" alt="圖片描述">
+						</a>
+						<div class="desc">精彩劇照4</div>
+					</div>
+				</div>
+				<div class="responsive">
+					<div class="img">
+						<a target="_blank" href="image/pic4.jpg">
+						<img src="image/pic4.jpg" alt="圖片描述">
+						</a>
+						<div class="desc">精彩劇照5</div>
+					</div>
+				</div>
+				<div class="responsive">
+					<div class="img">
+						<a target="_blank" href="image/pic5.jpg">
+						<img src="image/pic5.jpg" alt="圖片描述">
+						</a>
+						<div class="desc">精彩劇照6</div>
+					</div>
+				</div>	
+				<div class="responsive">
+					<div class="img">
+						<a target="_blank" href="image/pic6.jpg">
+						<img src="image/pic6.jpg" alt="圖片描述">
+						</a>
+						<div class="desc">幕後花絮1</div>
+					</div>
+				</div>
+				<div class="responsive">
+					<div class="img">
+						<a target="_blank" href="image/pic7.jpg">
+						<img src="image/pic7.jpg" alt="圖片描述">
+						</a>
+						<div class="desc">幕後花絮2</div>
+					</div>
+				</div>
+				<div class="responsive">
+					<div class="img">
+						<a target="_blank" href="image/pic8.jpg">
+						<img src="image/pic8.jpg" alt="圖片描述">
+						</a>
+						<div class="desc">幕後花絮3</div>
+					</div>
+				</div>
+				<div class="responsive">
+					<div class="img">
+						<a target="_blank" href="image/pic9.jpg">
+						<img src="image/pic9.jpg" alt="圖片描述">
+						</a>
+						<div class="desc">幕後花絮4</div>
+					</div>
+				</div>
+				<div class="responsive">
+					<div class="img">
+						<a target="_blank" href="image/pic10.jpg">
+						<img src="image/pic10.jpg" alt="圖片描述" width="300" height="200">
+						</a>
+						<div class="desc">幕後花絮5</div>
+					</div>
+				</div>
+				<div class="responsive">
+					<div class="img">
+						<a target="_blank" href="image/pic11.jpg">
+						<img src="image/pic11.jpg" alt="圖片描述">
+						</a>
+						<div class="desc">幕後花絮6</div>
+					</div>
+				</div>
+				<div class="responsive">
+					<div class="img">
+						<a target="_blank" href="image/pic12.jpg">
+						<img src="image/pic12.jpg" alt="圖片描述">
+						</a>
+						<div class="desc">幕後花絮7</div>
+					</div>
+				</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+	<div class="our-partners">
+		<div class="container" id="pictureBook">
+			<h3>音樂繪本</h3>
+            <div >bbbbb
+            	<h1>等學長資料</h1>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+	<div class="our-partners">
+		<div class="container" id="movieNovel">
+			<h3>電影小說</h3>
+            <div>ccccc
+            	<h1>等老師資料</h1>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+
+</body>
+</html>
